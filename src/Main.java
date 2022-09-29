065532b3b978386aa1bf2a0f15e43df8eb00d581
ilnurGatin <reilag@yandex.ru>
2022-09-30 00:15:15 +0500

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Task 1
+        //Task1
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
